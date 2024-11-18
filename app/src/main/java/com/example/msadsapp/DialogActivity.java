cf1f65a17a80aea60a3c9f03e5a141e5fd1fe170
Harpreet <harpreet.singh24@chicmic.co.in>
2024-11-18 17:48:31 +0530

    Dialog box module

---
+++ app/src/main/java/com/example/msadsapp/DialogActivity.java
@@ -0,0 +1,18 @@
+package com.example.msadsapp;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DialogActivity extends AppCompatActivity {
+
+    private static final long DISPLAY_DURATION_MS = 5000; // Show for 5 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_layout);
+
+        new Handler().postDelayed(this::finish, DISPLAY_DURATION_MS);
+    }
+
+}
